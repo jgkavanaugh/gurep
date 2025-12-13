873d79311c3617f96986c64ddb9aab87e9c6094e
= <jgkavanaugh@gmail.com>
2025-12-13 15:46:02 -0600

    added case insensitive flag and refactored line_reader function to be more idiomatically Rust

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::{self, Read, BufRead, BufReader};
-use regex::Regex;
+use std::io::{self, BufRead, BufReader};
+use regex::{RegexBuilder};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Pattern to look for
+    ///Pattern to look for
+    #[arg(short, long)]
     pattern: String,
-    /// Path to file
+    ///Path to file
+    #[arg(short, long)]
     file_path: Option<String>,
+    ///Case insensitive flag
+    #[arg(short, long)]
+    case_insensitive: bool,
+    //invert match
+    //v: ,
+    //stop after N matches
+    //m: ,
+    //print line numbers
+    //n: ,
 }
 
-fn line_reader(path: &Option<String>) -> io::Result<BufReader<Box<dyn Read>>> {
-    let file: Box<dyn Read> = match path {
-        Some(p) => Box::new(File::open(p)?),
-        None => Box::new(io::stdin()),
-    };
-    Ok(BufReader::new(file))
+fn line_reader(path: Option<&String>) -> io::Result<Box<dyn BufRead>> {
+    match path {
+        Some(p) => Ok(Box::new(BufReader::new(File::open(p)?))),
+        None => Ok(Box::new(BufReader::new(io::stdin().lock()))),
+    }
+}
+
+fn setup_regex(pattern: &str, case_insensitive: bool) -> regex::Regex {
+    if case_insensitive {
+        RegexBuilder::new(pattern).case_insensitive(true).build().unwrap()
+    }
+    else {
+        RegexBuilder::new(pattern).build().unwrap()
+    }
 }
 
 fn main() {   
@@ -25,10 +44,10 @@ fn main() {
     let args = Args::parse();
 
     //setup reader
-    let reader = line_reader(&args.file_path);
+    let reader = line_reader(args.file_path.as_ref());
 
     //setup regex
-    let re = Regex::new(&args.pattern).unwrap();
+    let re = setup_regex(&args.pattern, args.case_insensitive);
 
     //iterate through file looking for pattern
     for line in reader.expect("Errored reading line").lines() {
