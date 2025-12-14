c5115727704bc372da40d915affe82825571d456
= <jgkavanaugh@gmail.com>
2025-12-13 23:05:27 -0600

    added inverted matching and changed line_reader to accept &Path instead of &String. This is to avoid assumptions around UTF-8 usage.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use regex::{RegexBuilder};
+use std::path::{Path, PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -11,19 +12,20 @@ struct Args {
     pattern: String,
     ///Path to file
     #[arg(short, long)]
-    file_path: Option<String>,
+    file_path: Option<PathBuf>,
     ///Case insensitive flag
     #[arg(short, long)]
     case_insensitive: bool,
-    //invert match
-    //v: ,
+    ///Invert match
+    #[arg(short, long)]
+    invert_math: bool,
     //stop after N matches
     //m: ,
     //print line numbers
     //n: ,
 }
 
-fn line_reader(path: Option<&String>) -> io::Result<Box<dyn BufRead>> {
+fn line_reader(path: Option<&Path>) -> io::Result<Box<dyn BufRead>> {
     match path {
         Some(p) => Ok(Box::new(BufReader::new(File::open(p)?))),
         None => Ok(Box::new(BufReader::new(io::stdin().lock()))),
@@ -44,15 +46,24 @@ fn main() {
     let args = Args::parse();
 
     //setup reader
-    let reader = line_reader(args.file_path.as_ref());
+    let reader = line_reader(args.file_path.as_deref());
 
     //setup regex
     let re = setup_regex(&args.pattern, args.case_insensitive);
 
     //iterate through file looking for pattern
-    for line in reader.expect("Errored reading line").lines() {
-        if re.is_match(line.as_ref().unwrap()) {
-            println!("{}", line.expect("error"));
+    if args.invert_math {
+        for line in reader.expect("Errored reading line").lines() {
+            if !re.is_match(line.as_ref().unwrap()) {
+                println!("{}", line.expect("error"));
+            }
+        }
+    }
+    else {
+        for line in reader.expect("Errored reading line").lines() {
+            if re.is_match(line.as_ref().unwrap()) {
+                println!("{}", line.expect("error"));
+            }
         }
     }
 }
